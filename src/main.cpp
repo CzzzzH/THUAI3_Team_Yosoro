f18d839be83fa6f0e6cf3d8cf097a4f202467d8e
morphling233@gmail.com <morphling233@gmail.com>
2020-03-28 22:51:48 +0800

    添加项目文件。

---
+++ src/main.cpp
@@ -0,0 +1,74 @@
+#pragma warning(disable:4711)
+#pragma warning(disable:4710)
+#pragma warning(disable:5027)
+#pragma warning(disable:4623)
+#pragma warning(disable:4626)
+#pragma warning(disable:4668)
+#pragma warning(disable:4100)
+#pragma warning(disable:26495)
+#pragma warning(disable:26812)
+
+#include "Constant.h"
+#include <iostream>
+#include <sstream>
+#include <mutex>
+#include "player.h"
+#include "API.h"
+
+using namespace std;
+
+CAPI API;
+bool GameFinished = false;
+
+void *Ping(void *param)
+{
+	while (API.IsConnected())
+	{
+		if (!API.PrintBuffer())
+		{
+			API.Refresh();
+			//printf("Ping : %f ms\n", API.Ping);
+			Sleep(200);
+		}
+	}
+	return NULL;
+}
+int main(int argc, char *argv[])
+{
+	MapInfo::initialize_map();
+
+	char *agent_ip = (argv[1]);
+	int agent_port = atoi(argv[2]);
+	API.Initialize();
+	API.ConnectServer(agent_ip, agent_port);
+	pthread_t pt;
+	pthread_create(&pt, NULL, Ping, NULL);
+	pthread_detach(pt);
+	string message = "Connected!";
+
+	DebugFunc = DebugSilently;
+	//死循环执行玩家程序
+	while (!GameFinished) 
+	{
+		play();
+	}
+	getchar();
+	API.Quit();
+
+	// client聊天室测试
+	/*
+	while (API.IsConnected())
+	{
+		if (message == "quit")
+		{
+			API.Quit();
+		}
+		else
+			API.SendChatMessage(message + " from Agent " + to_string(API.AgentId) + " Player " + to_string(API.PlayerId));
+		cin >> message;
+	}
+	*/
+	cout << "Disconnected from server.\n";
+	getchar();
+	return 0;
+}
---
+++ src/player.cpp
@@ -0,0 +1,28 @@
+#include "API.h"
+#include "Constant.h"
+#include "player.h"
+#include <iostream>
+#include "OS_related.h"
+using namespace THUAI3;
+Protobuf::Talent initTalent = Protobuf::Talent::None;//指定人物天赋。选手代码必须定义此变量，否则报错
+void play()
+{
+	char c;
+	cin >> c;
+	switch (c)
+	{
+	case 'd':move(Protobuf::Direction::Right, 1000); break;
+	case 'e':move(Protobuf::Direction::RightUp, 1000); break;
+	case 'w':move(Protobuf::Direction::Up, 1000); break;
+	case 'q':move(Protobuf::Direction::LeftUp, 1000); break;
+	case 'a':move(Protobuf::Direction::Left, 1000); break;
+	case 'z':move(Protobuf::Direction::LeftDown, 1000); break;
+	case 'x':move(Protobuf::Direction::Down, 1000); break;
+	case 'c':move(Protobuf::Direction::RightDown, 1000); break;
+
+	default:
+		break;
+	}
+	
+	/*  玩家在这里写代码  */
+}
